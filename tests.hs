90b4f32662b95f7aa6a4ad6bf64d404bb632f59e
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-07-17 16:55:28 -0300

    Should have filtering for rolls

--- tests.hs
+++ tests.hs
@@ -1,10 +1,14 @@
 import Test.HUnit
 
 import Generator
+import DiceFilter
 
 testGenerateRollsWithOneDice = TestCase (assertEqual "generateRolls [4]" [[1], [2], [3], [4]] (generateRolls [4]))
 testGenerateRollsWithTwoDie = TestCase (assertEqual "generateRolls [2, 2]" [[1, 1], [1, 2], [2, 1], [2, 2]] (generateRolls [2, 2]))
 
-tests = TestList [testGenerateRollsWithOneDice, testGenerateRollsWithTwoDie]
+testDropOneWithOneRoll = TestCase (assertEqual "dropOne [1]" [] (dropOne [1]))
+testDropOneWithTwoRolls = TestCase (assertEqual "dropOne [1, 2]" [] (dropOne [2]))
+
+tests = TestList [testGenerateRollsWithOneDice, testGenerateRollsWithTwoDie, testDropOneWithOneRoll, testDropOneWithTwoRolls]
 
 main = runTestTT tests
