57887215b50f52482d5e2aafaf6402ac9cad25d3
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-07-17 02:13:53 -0300

    Should build tables with frequencies

--- dicestats.hs
+++ dicestats.hs
@@ -1,21 +1,69 @@
+import qualified Data.IntMap as IntMap
 import System.Environment
 import System.Exit
 import System.IO
 
-generateTable count sides = 0
+incrementValue :: IntMap.IntMap Int -> (Int, Int) -> Int -> IntMap.IntMap Int
+incrementValue map node side = IntMap.insert newKey newValue map
+    where
+        newKey = (fst node) + side
+        newValue = (snd node) + (IntMap.findWithDefault 0 newKey map)
 
-parseExpression args = putStrLn "Not done yet."
+addSideStep oldNodes newMap side
+    | null oldNodes = newMap
+    | otherwise = addSideStep (tail oldNodes) (incrementValue newMap (head oldNodes) side) side
 
-parse args
+-- The first roll of a 1d4 produces [(1, 1), (2, 1), (3, 1), (4, 1)]
+-- When rolling another 1d4, we run four times (one for each side) over the old states and combine these to the new states.
+-- After considering the 1 on the second dice, we get [(2, 1), (3, 1), (4, 1), (5, 1)].
+-- After considering the 2 on the second dice, we get [(2, 1), (3, 2), (4, 2), (5, 2), (6, 1)], and so on.
+addSide oldMap newMap side = addSideStep (IntMap.toList oldMap) newMap side
+
+nextTableStep old new cur max
+    | cur > max = new
+    | otherwise = nextTableStep old (addSide old new cur) (succ cur) max
+
+nextTable sides table = nextTableStep table IntMap.empty 1 sides
+
+generateTableStep count sides table
+    | count == 0 = table
+    | otherwise = generateTableStep (pred count) sides (nextTable sides table)
+
+generateTable :: Int -> Int -> IntMap.IntMap Int
+generateTable count sides = generateTableStep count sides (IntMap.fromList [(0, 1)])
+
+splitOnIncrementalFinish cur strs
+    | null cur = reverse strs
+    | otherwise = reverse ((reverse cur) : strs)
+
+splitOnIncremental :: String -> Char -> String -> [String] -> [String]
+splitOnIncremental str sym cur strs
+    | null str = splitOnIncrementalFinish cur strs
+    | head str == sym = splitOnIncremental (tail str) sym [] ((reverse cur) : strs)
+    | otherwise = splitOnIncremental (tail str) sym ((head str) : cur) strs
+
+-- Splits the string at the specified character.
+splitOn :: String -> Char -> [String]
+splitOn str sym = splitOnIncremental str sym [] []
+
+toInt str = read str :: Int
+
+parseRollExpression exp
+    | length split == 2 = print (generateTable (toInt (head split)) (toInt (last split)))
+    | otherwise = putStrLn "Unsupported roll expression."
+    where
+        split = splitOn exp 'd'
+
+parseFullCalculation args = parseRollExpression (head args)
+
+parseArguments args
     | null args || elem (head args) ["-h", "--help"] = printUsage >> exitWithSuccess
     | elem (head args) ["-v", "--version"] = printVersion >> exitWithSuccess
-    | otherwise = parseExpression args 
+    | otherwise = parseFullCalculation args 
 
 printVersion = putStrLn "Dice Statistics v1.0"
 printUsage = putStrLn "Usage: dicestats xdy [<|>|=] z"
 
 exitWithSuccess = exitWith ExitSuccess
 
-main = do
-    args <- getArgs
-    parse args
+main = getArgs >>= parseArguments
