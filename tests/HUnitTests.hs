f1f7e18f1c590cf2c344a2ac91fcdcec8ad2fcd7
Bernardo Sulzbach <mafagafogigante@gmail.com>
2017-01-09 12:28:36 -0200

    Should also have tests managed by Cabal

--- tests/HUnitTests.hs
+++ tests/HUnitTests.hs
@@ -1,14 +1,10 @@
 import Test.HUnit
 
 import Generator
-import DiceFilter
 
 testGenerateRollsWithOneDice = TestCase (assertEqual "generateRolls [4]" [[1], [2], [3], [4]] (generateRolls [4]))
 testGenerateRollsWithTwoDie = TestCase (assertEqual "generateRolls [2, 2]" [[1, 1], [1, 2], [2, 1], [2, 2]] (generateRolls [2, 2]))
 
-testDropOneWithOneRoll = TestCase (assertEqual "dropOne [1]" [] (dropOne [1]))
-testDropOneWithTwoRolls = TestCase (assertEqual "dropOne [1, 2]" [] (dropOne [2]))
-
-tests = TestList [testGenerateRollsWithOneDice, testGenerateRollsWithTwoDie, testDropOneWithOneRoll, testDropOneWithTwoRolls]
+tests = TestList [testGenerateRollsWithOneDice, testGenerateRollsWithTwoDie]
 
 main = runTestTT tests
